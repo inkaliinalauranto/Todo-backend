e1687beed462a8f7624e6b24e515d4cbe0e89785
Inka-Liina Lauranto <ilaurant@edu.lapinamk.fi>
2023-03-09 22:13:32 +0200

    Viimeisin lisäys

--- main.py
+++ main.py
@@ -2,48 +2,95 @@ from datetime import datetime
 from fastapi import FastAPI, Response # fastAPI on dokumentaatiosovellus
 from pydantic import BaseModel
 import sqlite3
+from fastapi.middleware.cors import CORSMiddleware
 
 con = sqlite3.connect("todos.sqlite", check_same_thread=False)
 
-sql_create_todo_table = "CREATE TABLE IF NOT EXISTS todo(id INTEGER PRIMARY KEY, title VARCHAR, description VARCHAR, done INTEGER)"
+sql_create_todo_table = "CREATE TABLE IF NOT EXISTS todo(id INTEGER PRIMARY KEY, title VARCHAR, description VARCHAR, done INTEGER, created_at INTEGER)"
 
 with con:
     con.execute(sql_create_todo_table)
+
 
 class TodoItem(BaseModel):
     id: int
     title: str
     done: bool
     description: str
+    created_at: int
+
 
 class NewTodoItem(BaseModel):
     title: str
     description: str
 
+
 app = FastAPI()
+
+# lisätään CORSMiddleware FastAPI-instanssille:
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=["*"], 
+    allow_credentials=True, 
+    allow_methods=["*"], 
+    allow_headers=["*"],
+)
+
 
 @app.on_event("shutdown")
 def database_disconnect():
     con.close()
 
+
 @app.get('/todos')  # tämän endpoint linkittyy siis fastapiin app:n kautta
-def get_todos(done: bool | None = None):
-    if done != None:
-        return f"Tässä palautetaan myöhemmin todot, joiden done-status on: {done}"
-    return "Tässä palautetaan myöhemmin todo-lista"
+def get_todos(response: Response, done: bool | None = None):
+    try:
+        with con:
+            if done != None:
+                cur = con.execute("SELECT id, title, description, done, created_at FROM todo WHERE done = ?", (int(done),))
+            else:
+                cur = con.execute("SELECT id, title, description, done, created_at FROM todo")
+            values: list[TodoItem] = []
+
+            for item in cur.fetchall():
+                id, title, description, done, created_at = item
+                todo = TodoItem(id=id, title=title, description=description, done=done != 0, created_at=created_at)
+                values.append(todo)
+
+            return values
+        
+    except Exception as e:
+        response.status_code = 500
+        return {"err": str(e)}
 
 '''done: bool | None = None, tällä kerrotaan FastAPI:lle että done on tyyppiä boolean ja 
 sen antaminen on vapaavalintaista. Oletusarvona done:lle asetetaan None. '''
 
 @app.get('/todos/{id}')
-def get_todo_by_id(id:int):
-    todo_item = TodoItem(id=id, title="testi", done=False)
-    return todo_item
+def get_todo_by_id(id: int, response: Response):
+    try:
+        with con:
+            cur = con.execute("SELECT id, title, description, done, created_at FROM todo WHERE id = ?", (id,))
+            result = cur.fetchone()  # tämä on nyt tuple-muotoinen
 
-''' Tämä aiemmin: 
-@app.post('/todos')
-def create_todo(todo_item: TodoItem):
-    return todo_item'''
+            if result == None:
+                response.status_code = 404
+                return {"err": f"Todo item with id {id} does not exists."}
+            
+            id, title, description, done, created_at = result
+
+            return TodoItem(
+                id=id, 
+                title=title, 
+                description=description, 
+                done=bool(done), 
+                created_at=created_at
+            )
+        
+    except Exception as e:
+        response.status_code = 500
+        return {"err": str(e)}
+
 
 @app.post('/todos')
 def create_todo(todo_item: NewTodoItem, response: Response):
@@ -52,9 +99,9 @@ def create_todo(todo_item: NewTodoItem, response: Response):
             dt = datetime.now()
             ts = int(datetime.timestamp(dt))
     
-            cur = con.execute("INSERT INTO todo(title, description, done) VALUES (?, ?, ?)", (todo_item.title, todo_item.description, int(False),))
+            cur = con.execute("INSERT INTO todo(title, description, done, created_at) VALUES (?, ?, ?, ?)", (todo_item.title, todo_item.description, int(False), ts,))
             response.status_code = 201
-            return TodoItem(id=cur.lastrowid, title=todo_item.title, done=False, description=todo_item.description)
+            return TodoItem(id=cur.lastrowid, title=todo_item.title, done=False, description=todo_item.description, created_at=ts)
         
     except Exception as e:
         response.status_code = 500
@@ -62,15 +109,63 @@ def create_todo(todo_item: NewTodoItem, response: Response):
 
 
 @app.put('/todos/{id}')
-def update_todo(id: int, todo_item: TodoItem):
-    return f"Myöhemmin tässä korvataan tietokannassa oleva todoitem uudella, jonka id on {id}"
+def update_todo(id: int, todo_item: TodoItem, response: Response):
+    try:
+        with con:
+            cur = con.execute(
+                "UPDATE todo SET title = ?, description = ?, done = ? WHERE id = ? RETURNING *", (todo_item.title, todo_item.description, todo_item.done, id,))
+            
+            result = cur.fetchone()
 
-@app.patch('/todos/{id}')
-def update_todo_status(id: int, todo_item: TodoItem):
-    return f"Myöhemmin tässä muokataan tietokannassa olevaa todoitemiä, jonka id on {id}"
+            if result == None:
+                response.status_code = 404
+                return {"err": f"Todo item with id {id} does not exist."}
+            
+            id, title, description, done, created_at = result
+
+            return TodoItem(
+                id=id, 
+                title=title, 
+                description=description, 
+                done=bool(done),
+                created_at=created_at
+            )
+    
+    except Exception as e:
+        response.status_code = 500
+        return {"err": str(e)}
+
+
+@app.patch('/todos/{id}/{done}')
+# Path parametrit otetaan argumenttina vastaan funktiossa, jossa niiden tietotyyppi määritetään
+def update_todo_status(id: int, done: bool, response: Response):
+    try:
+        with con:
+            cur = con.execute("UPDATE todo SET done = ? WHERE id = ? RETURNING done", (int(done), id,))
+            result = cur.fetchone()
+
+            if result == None:
+                response.status_code = 404
+                return {"err": f"Todo item with id {id} does not exists."}
+            
+            return {"done": bool(result[0])}
+    
+    except Exception as e:
+        response.status_code = 500
+        return {"err": str(e)}
+    
 
 @app.delete('/todos/{id}')
-def delete_todo(id: int):
-    return f"Myöhemmin tässä poistetaan tietokannasta todoitem, jonka id on {id}"
+def delete_todo(id: int, response: Response):
+    try:
+        with con:
+            cur = con.execute("DELETE FROM todo WHERE id = ?", (id,))
+            if cur.rowcount < 1:
+                response.status_code = 404
+                return {"err": f"Can't delete todo item, id {id} does not exist."}
+            
+            return "ok"
+    except Exception as e:
+        response.status_code = 500
+        return {"err": str(e)}
 
-#testi
